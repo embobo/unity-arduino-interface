88b8885eccf5905c25f660efb0080f8beadab911
Emma Bobola <embobola@gmail.com>
2017-03-10 16:35:26 -0500

    Added Custom Exceptions for Project

---
+++ fitboard-configuration/Assets/Scripts/EBLibrary/Resources/Exceptions/CustomExceptionClasses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[System.Serializable]
+public class InvalidTypeInitializerException : System.Exception
+{
+	public InvalidTypeInitializerException () : base() { }
+	public InvalidTypeInitializerException (string message) : base(message) { }
+	public InvalidTypeInitializerException (string message, System.Exception inner) : base(message, inner) { }
+
+	protected InvalidTypeInitializerException (System.Runtime.Serialization.SerializationInfo info,
+		System.Runtime.Serialization.StreamingContext context) { }
+}
